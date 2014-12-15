c2d20d31d75d6d0cb465acfbce04eae521f361cc
bryan hernandez <bryanhernandez@MacBook-Air-4.local>
2014-12-15 12:51:04 -0800

    add home.ejs

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var express =require("express")
-bodyParser = require("bodyParser")
-app = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs");
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express();
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 // our root route
-app.get("/", function (req, res){
-	res.render("sites/home");
-
+app.get("/", function (req, res) {
+  res.render("sites/home");
 });
 
-app.listen(3000, function(){
-	console.log(new.array)
-}
+app.listen(3000, function () {
+  console.log(new Array("*").join());
+  console.log("STARTED ON localhost:3000");
+})

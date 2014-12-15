adfcd29671f2bdcef6e6e07df36a605c9e0a18c8
bryan hernandez <bryanhernandez@MacBook-Air-4.local>
2014-12-15 12:24:04 -0800

    third commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define("user", {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password_digest: DataTypes.STRING,
+    BMI: DataTypes.STRING,
+    userName: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+
+  return user;
+};
